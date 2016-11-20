2de1e53707b0188fe3950579dde27d0439410e80
Richard Moss <richard@englishtutorslive.com>
2016-11-20 10:53:30 -0600

    adds fastclick to avoid 300ms click delay and changes layout of off-canvas menu

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        FastClick.attach(document.body);
     },
 
     // deviceready Event Handler
